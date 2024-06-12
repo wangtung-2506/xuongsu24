caf7a3347ce7d3b81c190c2bbbc0824943a3f215
wangtung-2506 <phungquangtung123vp@gmail.com>
2024-06-12 09:23:02 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+    content: ['./*.html'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#17AF26',
+                secondary: '#05422C',
+                'neutral-400': '#F4F4F4',
+            },
+            backgroundSize: {
+                '50%': '50%',
+            },
+        },
+    },
+    plugins: [],
+};
